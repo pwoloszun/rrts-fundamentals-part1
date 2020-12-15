097299f1dd0b4a2b557882cec6f7c9c7b9e372aa
Piotr Woloszun <piotr.woloszun@gmail.com>
2020-12-15 07:51:25 +0100

    updated forms tasks

--- src/features/04-forms/index.tsx
+++ src/features/04-forms/index.tsx
@@ -2,14 +2,24 @@ import React from 'react';
 
 import QuadraticEquation from './components/QuadraticEquation';
 // import StyledText from './components/StyledText';
+// import InterrelatedValidations from './components/InterrelatedValidations';
+import MyFormInterrelatedCtrls from './components/MyFormInterrelatedCtrls';
 
 export default function Forms() {
   return (
     <div>
       <h3>Forms</h3>
+
       <QuadraticEquation />
       <hr />
-      {/* <StyledText /> */}
+
+      {/* <StyledText/> */}
+      <hr />
+
+      {/* <InterrelatedValidations /> */}
+      <hr />
+
+      <MyFormInterrelatedCtrls />
     </div>
   );
 }
