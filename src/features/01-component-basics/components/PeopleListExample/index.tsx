686af765037ef1b7e0b3115a0c5dd267652aefe7
Piotr Woloszun <piotr.woloszun@gmail.com>
2021-01-17 23:06:09 +0100

    minor changes

---
+++ src/features/01-component-basics/components/PeopleListExample/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default class PeopleListExample extends React.Component {
+  render() {
+    // TODO
+    // const people = [
+    //   { id: 100, name: 'bob' },
+    //   { id: 200, name: 'ed' },
+    //   { id: 300, name: 'kate' },
+    // ];
+
+    return (
+      <div>
+        <h3>PeopleListExample</h3>
+      </div>
+    );
+  }
+}
